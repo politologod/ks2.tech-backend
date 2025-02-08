29b819bfae7b66bab30fccd2cab35e1960633b15
hbdev62 <hbdev62@gmail.com>
2025-02-08 16:08:54 -0400

    Creating the first commit, initializing the backend 4:08PM

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require("cors");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
+const PORT = process.env.PORT || 7777;
+app.listen(PORT, () => console.log(`🚀 Server Running Successfully on http://localhost:${PORT}`));
+
+
+
+app.get("/status", (req, res) => {
+	res.status(200).json({ status: "Server is running" });
+});
+
+
